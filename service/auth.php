5bdd075333f15b698037a0148993eea5bffeedfd
0rcaaa <iamsyarel@gmail.com>
2025-01-22 18:48:15 +0700

    delete unused function in auth.php

--- service/auth.php
+++ service/auth.php
@@ -11,10 +11,6 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
             form();
             break;
 
-        case 'data':
-            scan();
-            break;
-
         default:
         header ("location: ../index.php");
         break;
@@ -46,31 +42,3 @@ function form(){
     header ("location: ../index.php");
     }
 
-function scan(){
-    include "connection.php";
-    // check if the token is exist
-    $sql = "SELECT token FROM tamu WHERE token = '$item'";
-
-
-    if($conn->query($sql)->num_rows > 0){
-        // check if fid_tamu already exists in reports
-        $checkSql = "SELECT fid_tamu FROM reports WHERE fid_tamu = (SELECT id FROM tamu WHERE token = '$item')";
-        if($conn->query($checkSql)->num_rows == 0){
-            $sql = "INSERT INTO reports (fid_tamu, date, time, events_fid) VALUES (
-            (SELECT id FROM tamu WHERE token = '$item'), 
-            CURDATE(), 
-            CURTIME(), 
-            (SELECT fid_events FROM tamu WHERE token = '$item') 
-            )";
-            $conn->query($sql);
-            $_SESSION['success'] = 'data berhasil ditambahkan';
-        } else {
-            $_SESSION['error'] = 'kamu sudah scan';
-        }
-    } else {
-       $_SESSION['error'] = $conn->error;
-    }
-
-    // redirect to scan.php
-header("location: ../scan.php");
-}
